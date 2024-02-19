29cc330e35579a3a0a7976293e65b755edf8a553
Andreizayaz <poznavauka@mail.ru>
2024-02-19 18:09:10 +0300

    [feat]:add fonts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
 import "./scss/main.scss";
-import nature from "./assets/img/nature.jpg";
-import musicImg from './assets/img/music.jpg';
-
-const img = document.createElement("img");
-img.src = nature;
-document.body.appendChild(img);
-
-const music = document.createElement("img");
-music.src = musicImg;
-music.height=300;
-document.body.appendChild(music);
-
-const sum = (a:number, b:number) => a+b;
-
-console.log(sum(15,11))
