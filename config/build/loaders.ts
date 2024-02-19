864b83ab5fee859bb11fad6342561eebc538d978
Andreizayaz <poznavauka@mail.ru>
2024-02-19 16:13:25 +0300

    [feat]: add folder name for assets dist

--- config/build/loaders.ts
+++ config/build/loaders.ts
@@ -27,11 +27,17 @@ export const getLoaders = (mode: string) => {
   const imgLoader = {
     test: /\.(png|svg|jpg|jpeg|gif)$/i,
     type: "asset/resource",
+    generator: {
+      filename: "img/[name].[contenthash:8][ext]",
+    },
   };
 
   const fontsLoader = {
     test: /\.(woff|woff2|eot|ttf|otf)$/i,
     type: "asset/resource",
+    generator: {
+      filename: "fonts/[name].[contenthash:8][ext]",
+    },
   };
 
   return [tsLoader, cssLoader, imgLoader, fontsLoader];
